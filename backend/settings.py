3b959f511dd55431bd5d431452f54d71c2e40dc5
Iván González <ivan.gonzalez@campusfp.es>
2022-10-13 10:04:53 +0200

    asdaasda

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=hp54@@d043junp-*9z2bwh5=!+0w%1k6(e248a_1v5#+a%)mf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
